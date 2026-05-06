19579ea1cbd9c5530e802a0a4c624c1ddedcb460
Douglas Floriano <douglas97floriano@gmail.com>
2026-05-06 18:26:27 -0300

    refatora funcao de renderizacao de pensamentos para usar metodo separado

--- js/ui.js
+++ js/ui.js
@@ -7,18 +7,33 @@ const ui = {
 
         try {
             const pensamentos = await api.buscarPensamentos();
-            pensamentos.forEach((pensamento) => {
-                listaPensamentos.innerHTML += `
-                <li class="li-pensamento" data-id="${pensamento.id}">
-                    <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-                    <div class="pensamento-conteudo">${pensamento.conteudo}</div>
-                    <div class="pensamento-autoria">${pensamento.autoria}</div>
-                </li>
-                `;
-            });
+            pensamentos.forEach(ui.adicionarPensamentoNaLista);
         } catch {
             alert('Erro ao renderizar pensamentos');
         }
+    },
+
+    adicionarPensamentoNaLista(pensamento) {
+        const listaPensamentos = document.querySelector('#lista-pensamentos');
+        const li = document.createElement('li');
+        li.setAttribute('data-id', pensamento.id);
+        li.classList.add('li-pensamento');
+
+        const iconeAspas = document.createElement('img');
+        iconeAspas.src = 'assets/imagens/aspas-azuis.png';
+        iconeAspas.alt = 'Aspas Azuis';
+        iconeAspas.classList.add('icone-aspas');
+
+        const pensamentoConteudo = document.createElement('div');
+        pensamentoConteudo.textContent = pensamento.conteudo;
+        pensamento.classList.add('pensamento-conteudo');
+
+        const pensamentoAutoria = document.createElement('div');
+        pensamentoAutoria.textContent = pensamento.autoria;
+        pensamento.classList.add('pensamento-autoria');
+
+        li.append(iconeAspas, pensamentoConteudo, pensamentoAutoria);
+        listaPensamentos.append(li);
     },
 };
 
