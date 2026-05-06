f62e38003af438db36db76899f588421892269a7
Douglas Floriano <douglas97floriano@gmail.com>
2026-05-06 18:05:47 -0300

    Implementa funcao para salvar pensamentos na API

--- js/api.js
+++ js/api.js
@@ -8,6 +8,21 @@ const api = {
             throw error;
         }
     },
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(pensamento)
+            });
+            return response.json();
+        } catch (error) {
+            alert('erro');
+            throw error;
+        }
+    },
 };
 
 export default api;
